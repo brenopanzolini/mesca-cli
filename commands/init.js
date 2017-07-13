c3bc876c31c13675f07c5987c879b3a82f8fe55c
Breno Panzolini <bpanzolini@gmail.com>
2017-07-13 19:38:17 -0300

    Add option -d on init command

--- commands/init.js
+++ commands/init.js
@@ -4,19 +4,28 @@ const fs = require('fs-extra')
     , pwd = process.cwd();
 
 const generateDirStructure = (cmd) => {
-  cmd.log("Creating base directory structure...");
+  cmd.log("Creating base directory structure");
 
   util.configs.baseDirStructure.map((dir) => {
     fs.ensureDirSync(path.resolve(pwd, dir));
-    cmd.log(dir);
+    cmd.log('     ' + dir);
   });
 }
 
 const copyTemplates = (cmd) => {
-  cmd.log("Copying templates to private folder...");
+  cmd.log("Copying templates to private folder");
 
   fs.ensureDirSync(path.resolve(pwd, 'private/mesca/templates'));
   fs.copySync(path.resolve(__dirname, '../templates'), pwd + '/private/mesca/templates');
+}
+
+const deleteDefaultFiles = (cmd) => {
+  cmd.log("Removing default files");
+
+  util.configs.deleteDefaultFiles.map((file) => {
+    fs.removeSync(path.resolve(pwd, file));
+    cmd.log('    ' + file);
+  });
 }
 
 // Export command
@@ -24,6 +33,7 @@ module.exports = (vorpal) => {
   vorpal
     .command('init', 'Initialize project for scaffolding (execute from within the project).')
     .alias('i')
+    .option('-d, --delete', 'delete all default files')
     .validate(function (args) {
       if(!util.isMeteorProject) {
         this.log("Run 'init' within your Meteor project!");
@@ -35,6 +45,10 @@ module.exports = (vorpal) => {
     .action(function (args, callback) {
       generateDirStructure(this);
       copyTemplates(this);
+
+      if(args.options.delete){
+        deleteDefaultFiles(this);
+      }
 
       this.log('Initialized and scaffolding templates under private/mesca');
       callback();
