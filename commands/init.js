af99d2d49204af668156897db467572f9d143110
Breno Panzolini <bpanzolini@gmail.com>
2017-07-13 16:38:44 -0300

    Add init command

--- commands/init.js
+++ commands/init.js
@@ -1,11 +1,33 @@
-const fs = require('fs')
-    , pwd = process.cwd()
-    , util = require('../utils/util');
+const fs = require('fs-extra')
+    , path = require('path')
+    , util = require('../utils/util')
+    , pwd = process.cwd();
 
+const generateDirStructure = (cmd) => {
+  cmd.log("Creating base directory structure...");
+
+  util.configs.baseDirStructure.map((dir) => {
+    fs.ensureDirSync(path.resolve(pwd, dir));
+    cmd.log(dir);
+  });
+}
+
+const copyTemplates = (cmd) => {
+  cmd.log("Copying templates to private folder...");
+
+  fs.ensureDirSync(path.resolve(pwd, 'private/mesca/templates'));
+  fs.copySync(path.resolve(__dirname, '../templates'), pwd + '/private/mesca/templates');
+}
+
+// Command action
 const action = function(args, callback) {
   if(!util.isMeteorProject) {
     this.log("Run 'init' within your Meteor project!");
   } else {
+    generateDirStructure(this);
+    copyTemplates(this);
+
+    this.log('Initialized and scaffolding templates under private/mesca');
     callback();
   }
 }
@@ -13,6 +35,6 @@ const action = function(args, callback) {
 // Export command
 module.exports = (vorpal) => {
   vorpal
-  .command('init', 'Initialize project for scaffolding (execute from within the project).')
-  .action(action);
+    .command('init', 'Initialize project for scaffolding (execute from within the project).')
+    .action(action);
 }
--- utils/util.js
+++ utils/util.js
@@ -1,4 +1,7 @@
-const fs = require('fs')
-, pwd = process.cwd();
+const fs = require('fs-extra')
+    , path = require('path')
+    , pwd = process.cwd();
 
 exports.isMeteorProject = fs.existsSync(pwd + '/.meteor');
+
+exports.configs = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../utils/config.json')));
