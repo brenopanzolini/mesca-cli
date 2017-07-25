f383c3ecbde4756ab71daf6aee7fc41b0e253cd7
Breno Panzolini <bpanzolini@gmail.com>
2017-07-25 15:10:59 -0300

    Call rateLimit function inside methods.js

--- templates/api/methods.js
+++ templates/api/methods.js
@@ -1,3 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import __modelName from './__modelName';
 import rateLimit from '../../modules/rate-limit';
+
+// DDP rate limiter
+rateLimit({
+  methods: [],
+  limit: 5,
+  timeRange: 1000,
+});
