aabc9e2cdbda3bbb7ab2815e5dd6eaeaad54795c
Breno Panzolini <bpanzolini@gmail.com>
2017-08-07 21:16:38 -0300

    correct lint error

--- templates/api/publications.js
+++ templates/api/publications.js
@@ -1,4 +1,4 @@
 import { Meteor } from 'meteor/meteor';
 import __modelName from '../__modelName';
 
-Meteor.publish('__name', () => { return __modelName.find({}, { fields: __modelName.publicFields }); });
+Meteor.publish('__name', () => __modelName.find({}, { fields: __modelName.publicFields }));
